a450a57039dddd60ae70367bcae0c20d0272c0c8
Elijah Heath <ejheath8888@gmail.com>
2022-02-10 15:24:48 -0500

    string literal

--- Variable_Exercise/Program.cs
+++ Variable_Exercise/Program.cs
@@ -6,7 +6,17 @@ namespace Variable_Exercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string firstName = "John";
+
+            string sentence1 = "This is the first sentence.";
+
+            string sentence2 = "This is the second sentence";
+
+            string added = $"{sentence1} {sentence2}";
+
+            Console.WriteLine(added);
+
+
         }
     }
 }
